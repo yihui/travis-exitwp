819a4818dab831137ec044563a79704105593833
Yihui Xie <xie@yihui.name>
2018-09-19 23:56:15 -0500

    suppress stdout to make the travis log cleaner

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -ev
 ls ../*.xml || exit 0
 cp ../*.xml wordpress-xml/
 xmllint --noout wordpress-xml/*.xml
-python exitwp.py
+python exitwp.py > /dev/null
 zip -r build build
 curl -F "file=@build.zip" https://file.io
