ec5a5992d2bbe0b21eac2fbe8ffbed34a24a675d
Yihui Xie <xie@yihui.name>
2018-12-22 09:14:16 -0600

    the file.io API does not have a newline at the end of the result it returns, which probably confused Travis (#45)

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cp ../*.xml wordpress-xml/
 xmllint --noout wordpress-xml/*.xml
 python exitwp.py > /dev/null
 zip -r build build
-curl -F "file=@build.zip" https://file.io
+curl -F "file=@build.zip" https://file.io && printf "\nSee the link above of the form https://file.io/xxxxxx\n"
