ca6cf2befa6270e40b870ebcfff2d39e5cf9ebe2
Yihui Xie <xie@yihui.name>
2018-12-22 09:14:38 -0600

    quiet zip

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ ls ../*.xml || exit 0
 cp ../*.xml wordpress-xml/
 xmllint --noout wordpress-xml/*.xml
 python exitwp.py > /dev/null
-zip -r build build
+zip -q -r build build
 curl -F "file=@build.zip" https://file.io && printf "\nSee the link above of the form https://file.io/xxxxxx\n"
